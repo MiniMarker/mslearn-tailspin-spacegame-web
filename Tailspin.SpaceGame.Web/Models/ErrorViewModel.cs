7884a4f779340ee16a8b0f964e4e9344a3c4a202
MiniMarker <post@cmarker.no>
2020-02-16 19:04:00 +0100

    removed comment

--- Tailspin.SpaceGame.Web/Models/ErrorViewModel.cs
+++ Tailspin.SpaceGame.Web/Models/ErrorViewModel.cs
@@ -3,8 +3,6 @@ namespace TailSpin.SpaceGame.Web.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
-// Test
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
